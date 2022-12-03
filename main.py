cf9069f67e4bd085d15138cde9e9035f3a8cc8c6
Michał Hewelt <mhewelt9@gmail.com>
2022-12-03 19:48:34 +0100

    add recursion

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # ############## Our Blackjack House Rules #####################
 
 # # The deck is unlimited in size.
@@ -17,45 +16,53 @@ from random import choice
 
 def check_winner(user_score, computer_score):
     if user_score > 21:
-        return "You went over. You lose :("
+        print("You went over. You lose :(")
     elif user_score < computer_score < 22:
-        return "You lose :("
+        print("You lose :(")
     elif user_score > computer_score:
-        return "You win :)"
+        print("You win :)")
     else:
-        return "You draw."
+        print("You draw.")
 
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-print(logo)
+def blackjack():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    print(logo)
 
-player_cards = [choice(cards), choice(cards)]
-cpu_cards = [choice(cards), choice(cards)]
+    player_cards = [choice(cards), choice(cards)]
+    cpu_cards = [choice(cards), choice(cards)]
 
-cpu_score = sum(cpu_cards)
-while cpu_score < 17:
-    cpu_cards.append(choice(cards))
     cpu_score = sum(cpu_cards)
+    while cpu_score < 17:
+        cpu_cards.append(choice(cards))
+        cpu_score = sum(cpu_cards)
 
-player_score = sum(player_cards)
-keep_hitting = True
-while keep_hitting:
+    player_score = sum(player_cards)
+    keep_hitting = True
+    while keep_hitting:
 
-    print(f'    Your cards: {player_cards}, current score: {player_score}')
-    print(f"    Computer's first card: {cpu_cards[0]}")
-    hit = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        print(f'    Your cards: {player_cards}, current score: {player_score}')
+        print(f"    Computer's first card: {cpu_cards[0]}")
+        hit = input("Type 'y' to get another card, type 'n' to pass: ").lower()
 
-    if hit == 'y':
-        player_cards.append(choice(cards))
-        player_score = sum(player_cards)
-        if player_score >= 21:
+        if hit == 'y':
+            player_cards.append(choice(cards))
+            player_score = sum(player_cards)
+            if player_score >= 21:
+                keep_hitting = False
+        else:
             keep_hitting = False
+
+    print(f"    Your final hand: {player_cards}, final score: {player_score}")
+    print(f"    Computer's final hand: {cpu_cards}, final score: {cpu_score}")
+
+    check_winner(player_score, cpu_score)
+
+    play_again = input("Do you want to play another hand of Blackjack? Type 'y' or 'n': ").lower()
+    if play_again == 'y':
+        blackjack()
     else:
-        keep_hitting = False
-
-print(f"    Your final hand: {player_cards}, final score: {player_score}")
-print(f"    Computer's final hand: {cpu_cards}, final score: {cpu_score}")
-
-check_winner(player_score, cpu_score)
+        print('Thanks for playing!')
 
 
+blackjack()
